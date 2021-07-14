6c883286bafcfd8e161281342b4d7b92b3b4d075
Your Name <you@example.com>
2021-07-14 17:28:54 +0300

    commit

--- eShop.ShoppingCart.LocalStorage/ShoppingCart.cs
+++ eShop.ShoppingCart.LocalStorage/ShoppingCart.cs
@@ -1,9 +1,87 @@
-﻿using eShop.UseCases.CustomerPortal.PluginInterfaces.UI;
+﻿using eShop.CoreBusiness.Models;
+using eShop.UseCases.CustomerPortal.PluginInterfaces.DataStore;
+using eShop.UseCases.CustomerPortal.PluginInterfaces.UI;
+using Microsoft.JSInterop;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
 
 namespace eShop.ShoppingCart.LocalStorage
 {
     public class ShoppingCart : IShoppingCart
     {
-        
+        private const string constShoppingCart = "eShop.ShoppingCart";
+
+        private readonly IJSRuntime _jSRuntime;
+        private readonly IProductRepository _productRepository;
+
+        public ShoppingCart(IJSRuntime jSRuntime, IProductRepository productRepository)
+        {
+            _jSRuntime = jSRuntime;
+            _productRepository = productRepository;
+        }
+
+        public Task<Order> AddProductAsync(Product product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Order> DeleteProductAsync(int product)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task EmptyAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task GetOrderAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Order> PlaceOrderAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Order> UpdateOrderAsync(Order order)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<Order> UpdateQuantityAsync(int productId, int quantity)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private async Task<Order> GetOrder()
+        {
+            Order order;
+            // get the Order
+            var orderStr = await _jSRuntime.InvokeAsync<string>("localStorage.getItem", constShoppingCart);
+            if (string.IsNullOrWhiteSpace(orderStr))
+            {
+                order = new Order();
+                await SetOrder(order);
+            }
+            else
+                order = JsonConvert.DeserializeObject<Order>(orderStr);
+
+            // get LineItems 
+            foreach (var item in order.LineItems)
+            {
+                item.Product = _productRepository.GetProduct(item.ProductId);
+            }
+
+            return order;
+        }
+
+        private async Task SetOrder(Order order)
+        {
+            await _jSRuntime.InvokeVoidAsync("localStorage.setItem",
+                                             constShoppingCart,
+                                             JsonConvert.SerializeObject(order));
+        }
     }
 }
--- eShop.UseCases/CustomerPortal/PluginInterfaces/UI/IShoppingCart.cs
+++ eShop.UseCases/CustomerPortal/PluginInterfaces/UI/IShoppingCart.cs
@@ -1,7 +1,16 @@
-﻿namespace eShop.UseCases.CustomerPortal.PluginInterfaces.UI
+﻿using eShop.CoreBusiness.Models;
+using System.Threading.Tasks;
+
+namespace eShop.UseCases.CustomerPortal.PluginInterfaces.UI
 {
     public interface IShoppingCart
     {
-        
+        Task<Order> AddProductAsync(Product product);
+        Task<Order> DeleteProductAsync(int product);
+        Task EmptyAsync();
+        Task GetOrderAsync();
+        Task<Order> PlaceOrderAsync();
+        Task<Order> UpdateOrderAsync(Order order);
+        Task<Order> UpdateQuantityAsync(int productId, int quantity);
     }
 }
